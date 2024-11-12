a35fc7105163c3de3fea01b33633ced7425676b4
haarora <haarora@redhat.com>
2024-11-12 23:24:19 +0530

    adding demo-1

---
+++ demo-1/vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+    default = "us-east-1"
+}
+variable "AMIS" {
+    type = map(string)
+    default = {
+        us-east-1 = "ami-063d43db0594b521b"
+    }
+}
